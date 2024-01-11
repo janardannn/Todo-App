fda420d55a1a90495ac1ef65eb1fe4b43d2a4aca
janardannn <janardonhazarika7@gmail.com>
2024-01-11 16:02:06 +0530

    cosmetic changes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
     return (
-        <p className="text-[14px] text-center mt-36 mb-8">Copyright 2024 © <a href="https://github.com/janardannn/">github.com/janardannn</a></p>
+        <p className="text-[14px] text-center mt-[9rem] mb-8">Copyright 2024 © <a href="https://github.com/janardannn/">github.com/janardannn</a></p>
     )
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -15,8 +15,8 @@ export default function Header(props) {
 
     return (
         <div className="flex flex-col justify-center">
-            <h1 className="text-3xl text-center mt-8 mb-8">Todo App</h1>
-            <button onClick={handleLogout} onMouseOver={e => { setBtnText("Log out!") }} onMouseOut={e => { setBtnText(props.username) }} className="mb-8 mx-auto text-center border-2 border-slate-300 rounded-lg my-[0.3rem] w-[175px] hover:bg-red-600">{btnText}</button>
+            <h1 className="text-3xl text-center mt-16 mb-8">Todo App</h1>
+            <button onClick={handleLogout} onMouseOver={e => { setBtnText("Log out!") }} onMouseOut={e => { setBtnText(`🔐  ${props.username}`) }} className="mb-10 mx-auto text-center border-2 border-slate-300 rounded-lg my-[0.3rem] w-[175px] hover:bg-red-600">{btnText}</button>
         </div>
     )
 }
