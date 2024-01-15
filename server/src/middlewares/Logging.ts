56fef19dfb4c3120943336e42559a8f53815ce4a
janardannn <janardonhazarika7@gmail.com>
2024-01-15 17:37:33 +0530

    timed logging

--- server/src/middlewares/Logging.ts
+++ server/src/middlewares/Logging.ts
@@ -1,17 +1,25 @@
 import fs from "fs";
 import path from "path";
 
+import moment from "moment";
+import momentTz from "moment-timezone";
+
+momentTz.tz.setDefault("Asia/Kolkata");
+
+
 const logFilePath = path.resolve(__dirname, '../../LOGFILE');
 
 let count = 0;
 export function LogUserActivity(req: any, res: any, next: any) {
     count++;
 
+    let time = moment().format("HH:mm:ss DD/MM/YYYY")
+
     let user = req.body.user || req.query.user || req.body.username
 
-    console.log((user !== undefined ? "USER " + "[" + user + "]" : "USER") + " requested: " + "[" + req.method + "] " + req.originalUrl + " ; requests: " + count)
+    console.log(time + " " + (user !== undefined ? "USER " + "[" + user + "]" : "USER") + " requested: " + "[" + req.method + "] " + req.originalUrl + " ; requests: " + count)
 
-    let data_to_log: string = (user !== undefined ? "USER " + "[" + user + "]" : "USER") + " requested: " + "[" + req.method + "] " + req.originalUrl + " ; requests: " + count + "\n"
+    let data_to_log: string = time + " " + (user !== undefined ? "USER " + "[" + user + "]" : "USER") + " requested: " + "[" + req.method + "] " + req.originalUrl + " ; requests: " + count + "\n"
 
     try {
         fs.appendFile(logFilePath, data_to_log, function (err: any) {
